61e794d771af70cb57efaf1017ab598904d20db7
Trenton Wells <TJWELLS82787@gmail.com>
2023-03-20 16:35:59 -0500

    added fetchcall on app.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import FilterResults from './Components/FilterResults';
 import LocationSearch from './Components/LocationSearch';
 
 
-function App() {
-
+   
+  
+  const getEvents = () => {
+    fetch('https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=y4fgqxQmNBRy8u9y5IBixQXA56Zh0x3z&stateCode=IL',  {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    })
+    .then(response => response.json())
+    //.then(data => data.filter(x => x.city === usestate: city))
+    .then(data => { 
+        dispatch({ type: 'Eventlist', payload: data })
+    });
+  }
   return (
     <div className='App'>
       <header>
