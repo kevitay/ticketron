727bbd09b3d426e5a6a28d730fdbd5c3a65439a0
Trenton Wells <TJWELLS82787@gmail.com>
2023-03-21 16:14:37 -0500

    Price range display fixed

--- src/Components/EventItem.js
+++ src/Components/EventItem.js
@@ -2,6 +2,7 @@ import React from "react";
 // {Name : 'Event', Date: '1/2/2023', 
 // TicketPriceRange: '100-200', City: 'Bloomington', State: 'IL', Venue: 'Tom"s trunk barn'}
 function EventItem({event}) {
+    console.log(event)
     return (
         <div>
             <h2>Name: {event.name}</h2>
@@ -9,7 +10,7 @@ function EventItem({event}) {
             <p>City: {event._embedded.venues[0].city.name}</p>
             <p>State: {event._embedded.venues[0].state.name}</p>
             <p>Venue: {event._embedded.venues[0].name}</p>
-            {/* <p>Ticket price: {'$' + event.priceRanges[0].min + '-' + '$' + event.priceRanges[0].max}</p> */}
+            <p>Ticket price: {event.priceRanges ? '$' + event.priceRanges[0].min + '-' + '$' + event.priceRanges[0].max : 'No price to display'} </p>
             <img alt="Event logo"></img>
             <a href="?">link to seat selection page</a>
             <hr />
