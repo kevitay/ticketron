56998093b1cd6df0eb927ce11c64b8017ba313d6
Trenton Wells <TJWELLS82787@gmail.com>
2023-03-22 17:36:15 -0500

    Genre dropdown populating genres based on available events

--- src/Components/GenreDropdown.js
+++ src/Components/GenreDropdown.js
@@ -1,42 +1,40 @@
 import React, {useState} from "react";
-import GenreDropdown from "./GenreDropdown";
 
-function CategoryDropdown(props) {
+function GenreDropdown(props) {
     // console.log(props.eventsList)
-    const [filter, setFilter] = useState({Category: ''});
-    const segmentArray = props.eventsList.map((option) => option.classifications[0].segment.name);
+    const [filter, setFilter] = useState({genre: ''});
+    const segmentArray = props.eventsList.map((option) => option.classifications[0].genre.name);
     const uniqueSegments = [...new Set(segmentArray)]
 
-    const captureCategory = (event) => {
+    const captureGenre = (event) => {
         console.log('run');
         const newValue = event.target.value;
         console.log(newValue)
-        setFilter({Category: newValue});
+        setFilter({genre: newValue});
       }
 
     const handleFilter = (e) => {
         console.log("you got here")
         console.log(filter)
         e.preventDefault()
-        const CategoryEvents = props.eventsList.filter(x =>x.classifications[0].segment.name === filter.Category);
-            props.reducer({ type: 'eventList', payload: CategoryEvents })
+        const genres = props.eventsList.filter(x =>x.classifications[0].segment.name === filter.genre);
+            props.reducer({ type: 'eventList', payload: genres })
         };
 
     return (
       <div>
         <form onSubmit={(e) => {handleFilter(e)}}>
-        <select onChange={captureCategory}>
+        <select onChange={captureGenre}>
           {uniqueSegments.map((option) => (
             <option value={option}>{option}</option>
           ))}
         </select>
         <button type="submit" >Search</button>
         </form>
-        <GenreDropdown eventsList={props.eventsList} reducer={props.reducer}/>
       </div>
     );
   
   
 }
 
-export default CategoryDropdown; 
+export default GenreDropdown; 
