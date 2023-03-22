68585ed63577292f6ad98193394deaece315c9f8
austin.walker <austin.walker.wgwx@statefarm.com>
2023-03-22 11:38:26 -0500

    add event image to EventItem

--- src/Components/EventItem.js
+++ src/Components/EventItem.js
@@ -11,7 +11,7 @@ function EventItem({event}) {
             <p>State: {event._embedded.venues[0].state.name}</p>
             <p>Venue: {event._embedded.venues[0].name}</p>
             <p>Ticket price: {event.priceRanges ? '$' + event.priceRanges[0].min + '-' + '$' + event.priceRanges[0].max : 'No price to display'} </p>
-            <img alt="Event logo"></img>
+            <img alt="Event logo" src={event.images[0].url}></img>
             <a href="?">link to seat selection page</a>
             <hr />
         </div>
