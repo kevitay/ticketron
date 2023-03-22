3829c05967734edd3991fdaf1f281ba5d009000c
Trenton Wells <TJWELLS82787@gmail.com>
2023-03-22 13:59:01 -0500

    Got rid of duplicates in dropdown population

--- src/Components/CategoryDropdown.js
+++ src/Components/CategoryDropdown.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 function CategoryDropdown(props) {
-    console.log(props.eventsList.map((option) => option.classifications[0].segment.name))
+    console.log(props.eventsList)
     // [0].classifications[0].segment.name
     const segmentArray = props.eventsList.map((option) => option.classifications[0].segment.name);
+    const uniqueSegments = [...new Set(segmentArray)]
     // {props.eventsList.map((option) => option.classifications[0].segment.name)}
 
     return (
       <div>
         <select>
-          {segmentArray.map((option) => (
+          {uniqueSegments.map((option) => (
             <option value={option}>{option}</option>
           ))}
         </select>
