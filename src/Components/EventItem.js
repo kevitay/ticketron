6aaaa1ddad34fbf3f2fa9412d90c405aedcf01ce
austin.walker <austin.walker.wgwx@statefarm.com>
2023-03-22 12:42:52 -0500

    add localTime to EventItem

--- src/Components/EventItem.js
+++ src/Components/EventItem.js
@@ -3,16 +3,19 @@ import React from "react";
 // TicketPriceRange: '100-200', City: 'Bloomington', State: 'IL', Venue: 'Tom"s trunk barn'}
 function EventItem({event}) {
     // console.log(event)
+
     return (
         <div>
             <h2>Name: {event.name}</h2>
             <p>Date: {event.dates.start.localDate}</p>
+            <p>Time: {event.dates.start.localTime}</p>
             <p>City: {event._embedded.venues[0].city.name}</p>
             <p>State: {event._embedded.venues[0].state.name}</p>
             <p>Venue: {event._embedded.venues[0].name}</p>
             <p>Ticket price: {event.priceRanges ? '$' + event.priceRanges[0].min + '-' + '$' + event.priceRanges[0].max : 'No price to display'} </p>
             <img alt="Event logo" src={event.images[0].url}></img>
-            <a href="?">link to seat selection page</a>
+            <br></br>
+            <a target="_blank" href={event.url}>Buy Tickets</a>
             <hr />
         </div>
     )
