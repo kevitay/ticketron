9707791e1d3a5a72b24dfb45238cff09008cc409
Trenton Wells <TJWELLS82787@gmail.com>
2023-03-22 14:02:40 -0500

    Comit for jacobs change to Date time display update. Date time selectors added

--- src/Components/EventItem.js
+++ src/Components/EventItem.js
@@ -3,10 +3,12 @@ import React from "react";
 // TicketPriceRange: '100-200', City: 'Bloomington', State: 'IL', Venue: 'Tom"s trunk barn'}
 function EventItem({event}) {
     // console.log(event)
+    let eventDateTime = event.dates.start.dateTime;
+    let formattedDateTime = new Date(eventDateTime).toISOString().slice(0, 19).replace(/-/g, "/").replace("T", " ");
     return (
         <div>
             <h2>Name: {event.name}</h2>
-            <p>Date: {event.dates.start.localDate}</p>
+            <p>Date & Time: {formattedDateTime}</p>
             <p>City: {event._embedded.venues[0].city.name}</p>
             <p>State: {event._embedded.venues[0].state.name}</p>
             <p>Venue: {event._embedded.venues[0].name}</p>
