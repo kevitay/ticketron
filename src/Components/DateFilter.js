3ac6e7a412a29fc44f4f4eb7a30f78a59cf406f4
jacobopperman <jacob.opperman.vabqjb@statefarm.com>
2023-03-23 11:31:45 -0500

    Added button to apply date filter

--- src/Components/DateFilter.js
+++ src/Components/DateFilter.js
@@ -3,9 +3,11 @@ import { useState } from "react";
 
 function DateFilter({eventsList, reducer}) {
     let currentDate = new Date().toISOString().slice(0, 10);
+    let endOfYear = new Date(new Date().getFullYear(), 11, 31).toISOString().slice(0, 10);
+    let maxDateVal = new Date('2050-12-31').toISOString().slice(0, 10);
 
     const [minDate, setMinDate] = useState(currentDate);
-    const [maxDate, setMaxDate] = useState(currentDate);
+    const [maxDate, setMaxDate] = useState(endOfYear);
 
     const handleMinChange = (event) => {
         setMinDate(event.target.value);
@@ -15,12 +17,10 @@ function DateFilter({eventsList, reducer}) {
         setMaxDate(event.target.value);
     };
 
-    useEffect(() => {
+    const applyFilters = () => {
         let filteredResults = eventsList.filter(event => event.dates.start.localDate >= minDate && event.dates.start.localDate <= maxDate);
         reducer({ type: 'eventList', payload: filteredResults })
-    },
-        [minDate, maxDate]
-    )
+    }
 
     return (
         <div>
@@ -28,17 +28,17 @@ function DateFilter({eventsList, reducer}) {
 
             <input type="date" id="start" name="event-start"
                 value={minDate}
-                min={currentDate} max="2050-12-31"
+                min={currentDate} max={maxDateVal}
                 onChange={handleMinChange}>
             </input>
-            <br></br>
             <label for="end">End date:</label>
 
             <input type="date" id="end" name="event-start"
                 value={maxDate}
-                min={currentDate} max="2050-12-31"
+                min={currentDate} max={maxDateVal}
                 onChange={handleMaxChange}>
             </input>
+            <button type="button" onClick={applyFilters}>Apply Filter</button>
         </div>
     )
 }
