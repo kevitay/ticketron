35d5fdbafc5f5fdda0ef228b3b4b6bd6ff7a3f62
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-03-23 11:29:56 -0500

    Layout event divs

--- src/Components/EventItem.js
+++ src/Components/EventItem.js
@@ -6,22 +6,24 @@ function EventItem({event}) {
     let eventDateTime = event.dates.start.dateTime;
     let formattedDateTime = new Date(eventDateTime).toISOString().slice(0, 19).replace(/-/g, "/").replace("T", " ");
     return (
-        <div className="eventItem">
-            <div className="main-details">
-            <h2>{event.name}</h2>
-            <p>Date & Time: {formattedDateTime}</p>
-            </div>
-            <div className="location-details">
-            <p>City: {event._embedded.venues[0].city.name}</p>
-            <p>State: {event._embedded.venues[0].state.name}</p>
-            <p>Venue: {event._embedded.venues[0].name}</p>
-            </div>
-            <p>Ticket price: {event.priceRanges ? '$' + event.priceRanges[0].min + '-' + '$' + event.priceRanges[0].max : 'No price to display'} </p>
-            <img alt="Event logo" src={event.images[0].url}></img>
-            <br></br>
-            <a target="_blank" href={event.url}>Buy Tickets</a>
+      <div className="eventItem">
+        <div className="main-details">
+          <img alt="Event logo" src={event.images[0].url}></img>
+          <h2>{event.name}</h2><br/>
+          <p>{formattedDateTime}</p>
         </div>
-    )
+        <div className="location-details">
+          <p>
+            {event._embedded.venues[0].city.name}, {event._embedded.venues[0].state.name}
+          </p>
+          <p>Venue: {event._embedded.venues[0].name}</p>
+        </div>
+        <p>{event.priceRanges ? '$' + event.priceRanges[0].min + '-' + '$' + event.priceRanges[0].max : 'No price to display'} </p>
+        <a target="_blank" href={event.url}>
+          Buy Tickets
+        </a>
+      </div>
+    );
 }
 
 export default EventItem; 
